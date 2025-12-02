a9b650db318e0e2cb86decef599d90141d226f28
Sensei Intent Tensor <intent.tensor.theory@gmail.com>
2025-12-02 18:37:44 -0500

    Simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,9 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@types': path.resolve(__dirname, './src/types'),
-      '@services': path.resolve(__dirname, './src/services'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@constants': path.resolve(__dirname, './src/constants'),
-      '@content': path.resolve(__dirname, './src/content'),
     },
-  },
-  server: {
-    port: 5173,
-    open: true,
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
   },
 });
